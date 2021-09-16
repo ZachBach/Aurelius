2757afe082a50b49fb17a574d6d8a1aa18cc606c
Zach Auerbach <auernachzach@gmail.com>
2021-09-16 14:10:05 -0700

    3d cube done, as well as responsive

--- script.js
+++ script.js
@@ -45,3 +45,18 @@ function closeSubmenu(e) {
    
   /* Event listener */
   document.addEventListener("click", closeSubmenu, false);
+
+  // Swiper JS pagination
+  let swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
