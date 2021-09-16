5d57916c209371a3d7ee0c12815c181274c04dba
Zach Auerbach <auernachzach@gmail.com>
2021-09-16 13:42:15 -0700

    Did some minor refactoring, as well as started on the 3d  image cube component

--- script.js
+++ script.js
@@ -45,25 +45,3 @@ function closeSubmenu(e) {
    
   /* Event listener */
   document.addEventListener("click", closeSubmenu, false);
-
-  $("#os-phrases > h2")
-  .css('opacity', 1).lettering( 'words' )
-  .children( "span" ).lettering()
-  .children( "span" ).lettering(); 
-
-  // Text scramble effect
-  $(document).ready(function(){
-    var mouseX, mouseY;
-    var ww = $( window ).width();
-    var wh = $( window ).height();
-    var traX, traY;
-    $(document).mousemove(function(e){
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      traX = ((4 * mouseX) / 570) + 40;
-      traY = ((4 * mouseY) / 570) + 50;
-      console.log(traX);
-      $(".title").css({"background-position": traX + "%" + traY + "%"});
-    });
-  });
-  
